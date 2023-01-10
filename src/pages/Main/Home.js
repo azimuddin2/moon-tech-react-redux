70c6c7bfce8fe92f8a53875b50883c02abb5e0a7
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-01-10 10:33:50 +0600

    updated is home page

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
     const [products, setProducts] = useState([]);
 
     const filters = useSelector(state => state.filter.filters);
-    const {brands,stock} = filters;
-    
+    const { brands, stock } = filters;
+
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -19,30 +19,53 @@ const Home = () => {
 
     const activeClass = "bg-indigo-500 text-white border-white ";
 
+    let content;
+
+    if (products.length) {
+        content = products.map(product => <ProductCard
+            key={product._id}
+            product={product}
+        ></ProductCard>)
+    }
+
+    if (products.length && (stock || brands.length)) {
+        content = products
+            .filter((product) => {
+                if (stock) {
+                    return product.status === true;
+                }
+                return product;
+            })
+            .filter((product) => {
+                if (brands.length) {
+                    return brands.includes(product.brand);
+                }
+                return product;
+            })
+            .map((product) => <ProductCard key={product.model} product={product} />);
+    }
+
     return (
         <section className='mt-12'>
             <div className='flex justify-center gap-5'>
                 <button
-                onClick={() => dispatch(toggleStock())}
-                    className={`border px-4 py-2 rounded-full font-semibold ${
-                       stock ? activeClass : null
-                    }`}
+                    onClick={() => dispatch(toggleStock())}
+                    className={`border px-4 py-2 rounded-full font-semibold ${stock ? activeClass : null
+                        }`}
                 >
                     In Stock
                 </button>
                 <button
                     onClick={() => dispatch(toggleBrand('amd'))}
-                    className={`border px-4 py-2 rounded-full font-semibold ${
-                       brands.includes("amd") ? activeClass : null
-                     }`}
+                    className={`border px-4 py-2 rounded-full font-semibold ${brands.includes("amd") ? activeClass : null
+                        }`}
                 >
                     AMD
                 </button>
                 <button
                     onClick={() => dispatch(toggleBrand('intel'))}
-                    className={`border px-4 py-2 rounded-full font-semibold ${
-                        brands.includes("intel") ? activeClass : null
-                      }`}
+                    className={`border px-4 py-2 rounded-full font-semibold ${brands.includes("intel") ? activeClass : null
+                        }`}
                 >
                     Intel
                 </button>
@@ -50,10 +73,7 @@ const Home = () => {
 
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6'>
                 {
-                    products.map(product => <ProductCard
-                        key={product._id}
-                        product={product}
-                    ></ProductCard>)
+                    content
                 }
             </div>
         </section>
